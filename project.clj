4809c17a76d4ca7edaa554f2172eb23b12056765
Jarrad Hope <me@jarradhope.com>
2016-01-14 14:48:19 -0500

    update license to gplv2

--- project.clj
+++ project.clj
@@ -1,8 +1,8 @@
 (defproject narjure "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
-  :license {:name "Eclipse Public License"
-            :url  "http://www.eclipse.org/legal/epl-v10.html"}
+  :description "A Clojure implementation of the Non-Axiomatic Reasoning System proposed by Pei Wang."
+  :url "https://github.com/jarradh/narjure"
+  :license {:name "GNU General Public License 2.0"
+            :url  "http://www.gnu.org/licenses/old-licenses/gpl-2.0.html"}
   :dependencies [[org.clojure/clojure "1.7.0"]
                  [org.clojure/core.logic "0.8.10"]
                  [instaparse "1.4.1"]
