82989db0291c7891cabcbbf86a148ce8bdf294e6
Roman Volosovskyi <rasom@ukr.net>
2016-01-16 13:51:53 +0200

    parser tests

---
+++ test/narjure/test/narsese.clj
@@ -0,0 +1,34 @@
+(ns narjure.test.narsese
+  (:require [clojure.test :refer :all]
+            [narjure.narsese :refer :all]
+            [instaparse.core :refer [failure?]]))
+
+(defn narsese->clj [s] (:data (parse s)))
+(defn get-truth [s] (:truth (parse s)))
+
+(deftest test-parser
+  (is (= '([[conjunction [inheritance tim fish] [inheritance tom fish]]])
+         (narsese->clj "(&&, <tim --> fish>, <tom --> fish>).")))
+  (is (= '([[conjunction [inheritance tim fish] [inheritance tom fish]]])
+         (narsese->clj "(<tim --> fish> && <tom --> fish>).")))
+  (is (= '([[conjunction [inheritance tim fish] [inheritance tom fish]]])
+         (narsese->clj "((tim --> fish) && (tom --> fish)).")))
+  (is (= '([inheritance bird swimmer])
+         (narsese->clj "<bird --> swimmer>.")))
+  (is (= [1.0 0.9] (get-truth "<bird --> swimmer>. %1;0.9%"))))
+
+(deftest test-numbers-validation
+  (is (not (failure? (parse "<bird --> swimmer>. %1;0.9%"))))
+  ;check confidence
+  (is (failure? (parse "<bird --> swimmer>. %1;1.9%")))
+  (is (failure? (parse "<bird --> swimmer>. %1;1%")))
+  (is (failure? (parse "<bird --> swimmer>. %1;0%")))
+  (is (failure? (parse "<bird --> swimmer>. %1;0.0%")))
+  (is (failure? (parse "<bird --> swimmer>. %1;101%")))
+  (is (failure? (parse "<bird --> swimmer>. %1;55.55%")))
+  ;check frequence
+  (is (not (failure? (parse "<bird --> swimmer>. %0.1;0.9%"))))
+  (is (not (failure? (parse "<bird --> swimmer>. %0.0;0.9%"))))
+  (is (not (failure? (parse "<bird --> swimmer>. %0;0.9%"))))
+  (is (failure? (parse "<bird --> swimmer>. %5;0.9%")))
+  (is (failure? (parse "<bird --> swimmer>. %-1;0.9%"))))
