f4bf98b8ff5802c74d36a92f1fb81f934d7e783d
Roman Volosovskyi <rasom@ukr.net>
2016-01-21 11:42:45 +0200

    tests of default bag

--- src/narjure/bag.clj
+++ src/narjure/bag.clj
@@ -1,12 +1,13 @@
 (ns narjure.bag
-  (:require [clojure.data.priority-map :refer [priority-map-keyfn]]))
+  (:require [clojure.data.priority-map :refer [priority-map-keyfn-by]]))
 
 (defprotocol Bag
   (put-el [this item])
   (get-el
     [this]
     [this k])
-  (remove-el [this k]))
+  (remove-el [this k])
+  (count-els [this]))
 
 ;TODO must be discussed
 (defn randomize-priority [priority]
@@ -28,49 +29,11 @@
   (put-el [_ item]
     (DefaultBag. capacity (assoc-to-bag queue item capacity)))
   (get-el [_] (peek queue))
-  (get-el [_ key] (queue key))
-  (remove-el [_ key] (DefaultBag. capacity (dissoc queue key))))
+  (get-el [_ key] (when (contains? queue key) [key (queue key)]))
+  (remove-el [_ key] (DefaultBag. capacity (dissoc queue key)))
+  (count-els [_] (count queue)))
 
 (defn default-bag
   ([] (default-bag 100))
   ([capacity]
-   (DefaultBag. capacity (priority-map-keyfn :rand-priority))))
-
-#_(comment
-  (defrecord DefaultBag [capacity index queue]
-    Bag
-    (put-in [_ priority item]
-      (DefaultBag. capacity (assoc queue item priority)))
-    (get-out [_])
-    (count-eml [_] ()))
-
-  (defn default-bag []
-    (DefaultBag. 100 (priority-map)))
-
-  (defprotocol LimitedTable
-    (select [_ k])
-    (insert [_ k v])
-    (count-elms [_]))
-
-  (defrecord LimitedMap [col cap]
-    LimitedTable
-    (select [_ k] [(col k) (LimitedMap. (dissoc col k) cap)])
-    (insert [_ k v]
-      (let [ncol (if (>= (count col) cap)
-                   (into {} (butlast col))
-                   col)]
-        (LimitedMap. (assoc ncol k v) cap)))
-    (count-elms [_] (count col)))
-
-
-  (defn get-level [{:keys [levels]} {:keys [p]}]
-    (int (dec (Math/ceil (* p levels)))))
-
-  (defn select-level [{:keys [levels sel-factor]} {:keys [p]}]
-    (int (* (Math/pow (rand) sel-factor) levels)))
-
-  (defrecord PriorityTable [col cap levels sel-factor]
-    LimitedTable
-    (select [_ k])
-    (insert [_ k v])
-    (count-elms [_] (count col))))
+   (DefaultBag. capacity (priority-map-keyfn-by :rand-priority >))))
---
+++ test/narjure/test/bag.clj
@@ -0,0 +1,25 @@
+(ns narjure.test.bag
+  (:require [clojure.test :refer :all]
+            [narjure.bag :refer :all]))
+
+(defn abc-bag
+  ([] (abc-bag 3))
+  ([cap] (-> (default-bag cap)
+             (put-el {:key :a :priority 0.7})
+             (put-el {:key :b :priority 0.7})
+             (put-el {:key :c :priority 0.7}))))
+
+(def a-bag (put-el (default-bag) {:key :a :priority 0.7}))
+
+(deftest test-bag
+  (is (= 3 (count-els (abc-bag))))
+  (is (= 2 (count (-> (default-bag)
+                      (put-el {:key :a :priority 0.7})
+                      (put-el {:key :b :priority 0.7})
+                      (put-el {:key :a :priority 0.7})
+                      :queue))))
+  (is (= 2 (count-els (abc-bag 2))))
+
+  (is (= :a (-> a-bag get-el first)))
+  (is (= :a (-> a-bag (get-el :a) first)))
+  (is (nil? (get-el a-bag :b))))
