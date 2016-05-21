50694531c63c306d6403080bd273b7de24a6d255
TonyLo1 <tony_lofthouse@btinternet.com>
2016-05-21 21:24:15 +0100

    Delete test_utils.clj

--- test/nal/test/test_utils.clj
+++
@@ -1,9 +0,0 @@
-(ns nal.test.test-utils
-  (:require [clojure.test :refer [is]]
-            [clojure.core.logic :refer [run run*]]))
-
-(defmacro trun [result lvars & body]
-  `(is (= ~result (run 1 ~lvars ~@body))))
-
-(defmacro trun* [result lvars & body]
-  `(is (= ~result (run* ~lvars ~@body))))
