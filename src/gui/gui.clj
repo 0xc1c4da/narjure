6cd02b3791ba3b05f81396b6919e6954d8811ac0
patrick.hammer <patham9@gmail.com>
2016-05-29 16:42:30 +0200

    Hnav in own file.

--- src/gui/gui.clj
+++ src/gui/gui.clj
@@ -2,6 +2,6 @@
 
 (def gui-width 75)
 (def gui-height 50)
-(def nodes [{:name :button1 :px 0 :py -300 :onclick (fn [] (println "clicked")) :color [128 128 128]}])
+(def nodes [{:name :button1 :px 0 :py -300 :onclick (fn [] (println "clicked")) :backcolor [144 155 255]}])
 
 (def graph-gui [nodes [{:from :button1 :to :button1}] gui-width gui-height])
--- src/gui/lense.clj
+++ src/gui/lense.clj
@@ -2,53 +2,24 @@
   (:require [quil.core :as q]
             [quil.middleware :as m]
             [gui.actors :refer [graph-actors]]
-            [gui.gui :refer [graph-gui]]))
+            [gui.gui :refer [graph-gui]]
+            [gui.hnav :as hnav]))
 
-(def width 800)
-(def height 800)
-
-;HNAV API:
-(defn mouse-to-world-coord-x [x zoom difx]
-  (* (/ 1.0 zoom) (+ x (- difx) (- (/ width 2.0)))))
-
-(defn mouse-to-world-coord-y [y zoom dify]
-  (* (/ 1.0 zoom) (+ y (- dify) (- (/ height 2.0)))))
-
-(defn hnav-transform [{:keys [difx dify zoom]}]
-  (q/translate (+ difx (* 0.5 width))
-               (+ dify (* 0.5 height)))
-  (q/scale zoom zoom))
-;END HNAV
+(def graphs [[graph-actors] [graph-gui]])
 
 (defn setup []
-  ; initial state
-  {:x      0.0 :y 0.0 :r 10
-   :difx   (- (/ width 2))
-   :dify   (- (/ height 2))
-   :savepx 0.0
-   :savepy 0.0
-   :md     false
-   :zoom   1.0
-   :input-string ""})
+  (merge hnav/states {:input-string ""}))
 
 (defn update [state] state)
 
 (defn nameof [a]
   (if (string? a) a (name a)))
 
-(defn draw-actor [{:keys [name px py color]} node-width node-height]
-  (apply q/fill (if (= color nil) [255 255 255] color))
+(defn draw-actor [{:keys [name px py backcolor frontcolor]} node-width node-height]
+  (apply q/fill (if (= backcolor nil) [255 255 255] backcolor))
   (q/rect px py node-width node-height)
-  (apply q/fill [53 108 237])
+  (apply q/fill (if (= frontcolor nil) [0 0 0] frontcolor))
   (q/text (nameof name) (+ px 5) (+ py 10)))
-
-(def concept-size 15)
-(def concept-graph [[{:name "<a --> b>" :px 30 :py 370}
-                     {:name "a" :px 30 :py 330}
-                     {:name "b" :px 80 :py 330}]
-                    [{:from "<a --> b>" :to "a"}
-                     {:from "<a --> b>" :to "b"}]
-                    concept-size concept-size])
 
 (defn draw-graph [[nodes vertices node-width node-height]]
   (doseq [c vertices]
@@ -61,74 +32,32 @@
       (doseq [a nodes]
         (draw-actor a node-width node-height)))))
 
-(def graphs [[graph-actors] [concept-graph] [graph-gui]])
 (defn draw [state]
   (q/background 255)
   (q/reset-matrix)
-  (hnav-transform state)
+  (hnav/transform state)
   (doseq [[g] graphs]
     (draw-graph g))
-  (q/text (:input-string state) 400 -280)
-  ; (q/ellipse (mouse-to-world-coord-x (:x state) (:zoom state) (:difx state))
-  ;           (mouse-to-world-coord-y (:y state) (:zoom state) (:dify state)) (:r state) (:r state))
-  )
-
-;HNAV implementation
-(defn mouse-pressed [state event]                           ;also HGUI click check here
-  (doseq [g graphs]
-    (doseq [[V E w h] g]
-      (doseq [v V]
-        (let [px (:px v)
-              py (:py v)
-              mousex (mouse-to-world-coord-x (:x event) (:zoom state) (:difx state))
-              mousey (mouse-to-world-coord-y (:y event) (:zoom state) (:dify state))]
-          (if (and (not (= (:onclick v) nil)) (> mousex px) (> mousey py)
-                   (< mousex (+ px w)) (< mousey (+ py h)))
-            ((:onclick v)))))))
-  (assoc state :savepx (:x event) :savepy (:y event) :md true))
-
-(defn mouse-dragged [state event]
-  (-> state
-      (assoc :difx (+ (:difx state) (- (:x event) (:savepx state)))
-             :dify (+ (:dify state) (- (:y event) (:savepy state)))
-             :savepx (:x event)
-             :savepy (:y event))))
-
-(def scrollcamspeed 1.1)
-(defn mouse-wheel [state mouse-scroll]
-  (let [zoom-before (:zoom state)
-        state2 (if (> mouse-scroll 0)
-                 (assoc state :zoom (/ (:zoom state) scrollcamspeed))
-                 (assoc state :zoom (* (:zoom state) scrollcamspeed)))]
-    (-> state2
-        (assoc :difx (* (:difx state2) (/ (:zoom state2) zoom-before)))
-        (assoc :dify (* (:dify state2) (/ (:zoom state2) zoom-before))))))
-;END HNAV
+  (q/text (:input-string state) 400 -280))
 
 (defn key-pressed [state event]
   (let [name (name (:key event))
         code (:key-code event)]
     (assoc state :input-string (str (if (not (= code 8))
-                                      (:input-string state)
-                                      "")
+                                      (:input-string state) "")
                                     (if (not (= name "shift"))
                                       (if (not (= code 8))
-                                        name
-                                        "")
+                                        name "")
                                       "")))))
 
-(defn key-pressedgg [state event]
-  (println event)
-  state)
-
 (q/defsketch example
-             :size [width height]
+             :size [hnav/width hnav/height]
              :setup setup
              :draw draw
              :update update
-             :mouse-pressed mouse-pressed
-             :mouse-dragged mouse-dragged
-             :mouse-wheel mouse-wheel
+             :mouse-pressed (partial hnav/mouse-pressed graphs)
+             :mouse-dragged hnav/mouse-dragged
+             :mouse-wheel hnav/mouse-wheel
              :key-pressed key-pressed
              :middleware [m/fun-mode]
              :title "OpenNARS 2.0.0: Lense")
