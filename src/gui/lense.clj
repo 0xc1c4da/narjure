0eab98e45e50610142b331ee0f3288ec54696dce
patrick.hammer <patham9@gmail.com>
2016-05-29 05:48:33 +0200

    Ability to type in Sentence parser actor visualization.

--- src/gui/lense.clj
+++ src/gui/lense.clj
@@ -26,12 +26,13 @@
    :savepx 0.0
    :savepy 0.0
    :md     false
-   :zoom   1.0})
+   :zoom   1.0
+   :input-string ""})
 
 (defn update [state]
   state)
 
-(defn draw-actor [{:keys [name px py node-width node-height]}]
+(defn draw-actor [{:keys [name px py]} node-width node-height]
   (apply q/fill [255 255 255])
   (q/rect px py node-width node-height)
   (apply q/fill [53 108 237])
@@ -40,16 +41,20 @@
 (def actor-level-width 175)
 (def actor-level-height 100)
 
-(def nodes [{:name "Concept Manager" :px 0 :py 0 :node-width actor-level-width :node-height actor-level-height}
-            {:name "Concepts" :px 0 :py 300 :node-width actor-level-width :node-height actor-level-height}
-            {:name "Task Dispatcher" :px 200 :py 0 :node-width actor-level-width :node-height actor-level-height}
-            {:name "Sentence Parser" :px 200 :py -300 :node-width actor-level-width :node-height actor-level-height}
-            {:name "Task Creator" :px 400 :py -150 :node-width actor-level-width :node-height actor-level-height}
-            {:name "Operator Executor" :px -350 :py -150 :node-width actor-level-width :node-height actor-level-height}
-            {:name "Event Buffer" :px 200 :py 150 :node-width actor-level-width :node-height actor-level-height}
-            {:name "General Inferencer" :px 400 :py 300 :node-width actor-level-width :node-height actor-level-height}
-            {:name "Event Selector" :px 600 :py 150 :node-width actor-level-width :node-height actor-level-height}
-            {:name "Concept Selector" :px 400 :py 450 :node-width actor-level-width :node-height actor-level-height}])
+(def gui-width 20)
+(def gui-height 20)
+(def gui [{:name ""}])
+
+(def nodes [{:name "Concept Manager" :px 0 :py 0}
+            {:name "Concepts" :px 0 :py 300}
+            {:name "Task Dispatcher" :px 200 :py 0}
+            {:name "Sentence Parser" :px 400 :py -300}
+            {:name "Task Creator" :px 400 :py -150}
+            {:name "Operator Executor" :px -350 :py -150}
+            {:name "Event Buffer" :px 200 :py 150}
+            {:name "General Inferencer" :px 400 :py 300}
+            {:name "Event Selector" :px 600 :py 150}
+            {:name "Concept Selector" :px 600 :py 450}])
 
 (def vertices [{:from "Concept Manager" :to "Task Dispatcher"}
                {:from "Concepts" :to "Concept Manager"}
@@ -63,32 +68,34 @@
                {:from "General Inferencer" :to "Task Creator"}
                {:from "Task Creator" :to "Task Dispatcher"}])
 
-(def graph-1 [nodes vertices])
+(def graph-actors [nodes vertices actor-level-width actor-level-height])
 
 (def concept-size 15)
-(def concept-graph [[{:name "<a --> b>" :px 30 :py 370 :node-width concept-size :node-height concept-size}
-                     {:name "a" :px 30 :py 330 :node-width concept-size :node-height concept-size}
-                     {:name "b" :px 80 :py 330 :node-width concept-size :node-height concept-size}]
+(def concept-graph [[{:name "<a --> b>" :px 30 :py 370}
+                     {:name "a" :px 30 :py 330}
+                     {:name "b" :px 80 :py 330}]
                     [{:from "<a --> b>" :to "a"}
-                     {:from "<a --> b>" :to "b"}]])
+                     {:from "<a --> b>" :to "b"}]
+                    concept-size concept-size])
 
-(defn draw-graph [[nodes vertices]]
+(defn draw-graph [[nodes vertices node-width node-height]]
   (doseq [c vertices]
     (let [left (first (filter #(= (:from c) (:name %)) nodes))
           right (first (filter #(= (:to c) (:name %)) nodes))
-          pxtransform (fn [x] (+ (:px x) (/ (:node-width x) 2.0)))
-          pytransform (fn [y] (+ (:py y) (/ (:node-height y) 2.0)))]
+          pxtransform (fn [x] (+ (:px x) (/ node-width 2.0)))
+          pytransform (fn [y] (+ (:py y) (/ node-height 2.0)))]
       (q/line (pxtransform left) (pytransform left)
               (pxtransform right) (pytransform right))
       (doseq [a nodes]
-        (draw-actor a)))))
+        (draw-actor a node-width node-height)))))
 
 (defn draw [state]
   (q/background 255)
   (q/reset-matrix)
   (hnav-transform state)
-  (draw-graph graph-1)
+  (draw-graph graph-actors)
   (draw-graph concept-graph)
+  (q/text (:input-string state) 400 -280)
   ;(draw-graph concept-graph)                                ;TODO just draw elements recursively
                                         ;adding the position of the parent, this is easy
 
@@ -118,6 +125,22 @@
         (assoc :dify (* (:dify state2) (/ (:zoom state2) zoom-before))))))
 ;END HNAV
 
+(defn key-pressed [state event]
+  (let [name (name (:key event))
+        code (:key-code event)]
+    (assoc state :input-string (str (if (not (= code 8))
+                                      (:input-string state)
+                                      "")
+                                    (if (not (= name "shift"))
+                                      (if (not (= code 8))
+                                        name
+                                        "")
+                                      "")))))
+
+(defn key-pressedgg [state event]
+  (println event)
+  state)
+
 (q/defsketch example
              :size [width height]
              :setup setup
@@ -126,5 +149,6 @@
              :mouse-pressed mouse-pressed
              :mouse-dragged mouse-dragged
              :mouse-wheel mouse-wheel
+             :key-pressed key-pressed
              :middleware [m/fun-mode]
              :title "OpenNARS 2.0.0: Lense")
